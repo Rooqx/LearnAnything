902f3b7b81c3cfde5765551b6855ba87c264353c
Rooqx <ashfaga2005f@email.com>
2026-05-23 21:51:55 +0100

    Creating new ui

--- src/app/dashboard/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import ScoopedFrame from "@/src/components/layout/ScoopedFrame";
-
-export default function DashboardLayout({ children }: { children: ReactNode }) {
-  return <ScoopedFrame isDashboard>{children}</ScoopedFrame>;
-}
--- src/components/dashboard/crtCourse.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import sendMsg from "@/src/app/create-course/action";
-import CreateCourse from "./CreateCourse";
-
-export default function CreateCoursePageSub() {
-  return <CreateCourse onSend={sendMsg} />;
-}
